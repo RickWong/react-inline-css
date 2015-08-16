6ed94f12b443b0de0338798f84195c6fd426bcf7
Rick <wong@mollie.com>
2015-08-16 23:52:52 +0200

    Fixes #6: not namespacing the first embedded rule.

--- src/react-inline-css.js
+++ src/react-inline-css.js
@@ -22,7 +22,7 @@ var InlineCss = React.createClass({
 			replace(/}\s*/ig, '\n}\n').
 			// Regular rules are namespaced.
 			replace(
-			/(^|}|;|,)\s*([&a-z0-9\-_\.:#\(\),>*\s]+)\s*(\{)/ig,
+			/(^|{|}|;|,)\s*([&a-z0-9\-_\.:#\(\),>*\s]+)\s*(\{)/ig,
 			function (matched) {
 				return matched.replace(componentName, "#" + namespace);
 			}
